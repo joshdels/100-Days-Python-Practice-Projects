0330b90119a03bbb8a208c86b1100863f0adcb25
joshdels <deleonjoshy2@gmail.com>
2025-01-13 15:38:20 +0800

    uploaded files

---
+++ Day_01_BandNameGenerator.py
@@ -0,0 +1,10 @@
+'''
+Some Warm ups and speed running Older Python Projects 
+1/13/2025
+'''
+
+print('Welcome to the Band Name Generator')
+city = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+
+print("Your band name could be " + city.title() +" "+pet.title())
---
+++ Day_02_Tip_Calculator.py
@@ -0,0 +1,14 @@
+'''
+Drinking with friends with lunch
+'''
+
+print('Welcome to the tip calculator!')
+bill = input('What was the total bill? $')
+tip = input("How much tip would you like to give? 10, 12, or 15? ")
+people = input("How many people to split the bill? ")
+
+# Math Calculator
+tip_to_pay = (float(bill) * (float(tip)/100)) / int(people)
+
+# Results
+print(f'Each person should pay: ${tip_to_pay}')
