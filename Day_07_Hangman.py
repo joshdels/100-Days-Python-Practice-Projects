954c25ef78d1f084f11c99adbe7fe5500e40fab2
Joshua De Leon <deleonjoshy2@gmail.com>
2025-01-19 03:37:31 +0800

    hangman_practice

--- Day_07_Hangman.py
+++ Day_07_Hangman.py
@@ -60,31 +60,43 @@ stages = [r'''
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 
+game_is_on = True
+lives = 6
+
 word_list = ["aardvark", "baboon", "camel"]
 
 
-choosen_word = random.choice(word_list)
-letter_list = list(choosen_word)
+hidden_word = random.choice(word_list)
+letter_list = list(hidden_word)
+display_word = ["_"] *len(hidden_word)
 
-# debug word selected
-print(choosen_word) 
+while game_is_on and lives >= 0:
+    print(" ".join(display_word))
+    
 
-# User Guess section
-guess = input("Make a letter guess").lower()
+    guess = input("Make a letter guess: ").lower()
 
-for letter in letter_list:
-    if guess == letter:
-        print(letter)
-        # letter_list.pop(letter)
-        break
+    if guess in letter_list:
+      # removing of elements
+      for letter in letter_list:
+          if guess == letter:
+              letter_list.remove(letter)
+              display_word[letter] = guess # need to debug this part
     else:
-        print('no letters found')
+      print("Wrong!")
+      lives -= 1
+
+      #print status
+      print(stages.pop())
+
+    if len(letter_list) <= 0:
+      game_is_on = False
+      print("No more letters left")
+      print("You survived")
+
+print("Game Over!")
 
 
 
 
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
