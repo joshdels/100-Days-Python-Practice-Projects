c6ebdfa4e38fd07bb495c58c7fb0bb89829427c7
Joshua De Leon <deleonjoshy2@gmail.com>
2025-01-22 17:43:17 +0800

    Update Day_07_Hangman.py

--- Day_07_Hangman.py
+++ Day_07_Hangman.py
@@ -68,57 +68,26 @@ word_list = ["aardvark", "baboon", "camel"]
 
 hidden_word = random.choice(word_list)
 letter_list = list(hidden_word)
-<<<<<<< Updated upstream
-
 #this will create an empty list na naay length
 display_word = ['_' for _ in hidden_word]
-
 
 while game_is_on and lives > 0:
   
   print(f"Word: {' '.join(display_word)}")
   print(f"Lives left: {lives}")
   
-  
   guess = input("Make a letter guess: ").lower()
 
   if guess in letter_list:
-    # removing of elements, enumerate kay i run niya first the hidden it and letter ug guest ba nako is dili compatible
-      #if mu run siya first kay, i display niya ang empty letter, the kay iyang i remove ang letter, after sa for loop
-      # iyang ipakita ang letter_list which is confusing sa akoa na part
-    for index,letter in enumerate(hidden_word):
+    # removing of elements
+    for index, letter in enumerate(hidden_word):
         if guess == letter:
           display_word[index] = letter
           letter_list.remove(letter)
-    
-    # letter_list = [letter for letter in letter_list if letter != guess]
-            
   else:
     print("Wrong!")
     lives -= 1
     if stages:
-    
-=======
-display_word = []
-# display_word = ["_"] *len(hidden_word)
-
-while game_is_on and lives >= 0:
-    # print(" ".join(display_word))
-    
-
-    guess = input("Make a letter guess: ").lower()
-
-    if guess in letter_list:
-      # removing of elements
-      for letter in letter_list:
-          if guess == letter:
-              letter_list.remove(letter)
-               guess # need to debug this part
-    else:
-      print("Wrong!")
-      lives -= 1
-
->>>>>>> Stashed changes
       #print status
       print(stages.pop())
       
