abd7e2d98cec95583553fc651b142057b7542213
Joshua De Leon <deleonjoshy2@gmail.com>
2025-01-31 22:47:04 +0800

    Update Day_02_Tip_Calculator.py

--- Day_02_Tip_Calculator.py
+++ Day_02_Tip_Calculator.py
@@ -3,8 +3,8 @@ Drinking with friends with lunch
 '''
 
 print('Welcome to the tip calculator!')
-bill = input('What was the total bill? $')
-tip = input("How much tip would you like to give? 10, 12, or 15? ")
+bill = input('What was the total bill?: $')
+tip = input("How much tip would you like to give? 10, 12, or 15?: ")
 people = input("How many people to split the bill? ")
 
 # Math Calculator
