996ed59d59a0e62329307cac54e2e6b6f063d59e
joshdels <deleonjoshy2@gmail.com>
2025-03-15 18:36:07 +0800

    done guess number game

---
+++ Day 12/guessingNumberAssignmnet.py
@@ -0,0 +1,67 @@
+import random
+from art import logo
+
+game_on = True
+
+def diff():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    attempts = 0
+    if difficulty == 'hard':
+        attempts += 5
+        return attempts
+    elif difficulty == 'easy':
+        attempts += 10
+        return attempts
+        
+# selecting the number
+def random_number():
+    '''Chooses the random Number'''
+    number_to_guess = random.randint(0, 100)
+    return number_to_guess
+
+def checking_number(number, random_num):
+    '''This will check the number if it is close the the subjected random number'''
+    if random_num == number:
+        print("Congraulations! You guessed the correct answer")
+        return True
+    elif number > random_num:
+        print("Too High")
+        return False
+    elif number < random_num:
+        print("Too Low")
+        return False  
+ 
+# game_start 
+
+def game():
+    '''This will start the guessing game'''
+    print(logo)
+    print("Welcome to the Number Guessing Game!\n I'm thinking of a number between 1 and 100: ")
+    ran_num = random_number()
+
+    attempts = diff()
+
+    while  attempts > 0:
+        my_num = int(input("What is your number: (1 - 100): "))
+        if checking_number(my_num, ran_num):
+            break
+        print(f"Attemps left {attempts-1}")
+        attempts -= 1
+
+    if attempts == 0:
+        print(f"GG try again mate, right answer {ran_num}")
+        
+# game start       
+while game_on:
+    decision = input("Would you like to play? (Y/N): ").upper()
+    if decision == 'Y':
+        print("\n"*20)
+        game()
+    elif decision == 'N':
+        print("Thank You for playing the game")
+        game_on = False
+    else: 
+        print("Please enter a valid option (Y/N): ")
+        
+# missing docstring josh huhu
+
