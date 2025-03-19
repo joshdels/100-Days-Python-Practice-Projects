cbf622d943c80a2838009ed049160a48919d3910
joshdels <deleonjoshy2@gmail.com>
2025-03-19 23:43:10 +0800

    better solution, hmmm sa pag deduct lng sa resources medyo kulang ko heheh

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -1,16 +1,44 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# constructor
 menu = Menu()
-menu.get_items()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine() 
 
-drink = input("drinks?")
+coffee = menu.get_items()
+coffee_list = coffee.split("/")
 
-print(menu.find_drink(drink))
+#choose drink
 
-coffee_machine = CoffeeMaker()
-coffee_machine.report()
-print(coffee_machine.is_resource_sufficient())
+is_choosing_coffee = True
+checking = True
+while is_choosing_coffee:
+    while checking:
+        user_coffee = input(f"what is your drink print({coffee})")
+        if user_coffee == 'report':
+            coffee_maker.report()
+            money_machine.report()
+        elif user_coffee in coffee_list:
+            # cost of coffee
+            coffee_items = menu.find_drink(user_coffee)
+            cost_of_coffee = coffee_items.cost
+            break
+        else:
+            print(f"Please enter correctly {coffee}")
 
-#  wala ko kasabottt huhuhuuuuu need nakog practice whwahhaha
+    # coffee proceeding
+    if coffee_maker.is_resource_sufficient(coffee_items):
+        # coffee_maker.make_coffee()
+        print("resources are available")
+        #make payment
+        money_machine.make_payment(cost_of_coffee)
+        #compute remaining resources
+        
+    else:
+        print("insufficint resources returning your money")
+    
+
+
+# dili lagi naga bawas ang resouces??? hmmm
