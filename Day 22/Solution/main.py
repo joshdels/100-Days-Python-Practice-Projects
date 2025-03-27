01f97c4b28000a12dfe0da6618834223534e32d6
joshdels <deleonjoshy2@gmail.com>
2025-03-27 23:18:50 +0800

    practice solution pong

--- Day 22/Solution/main.py
+++ Day 22/Solution/main.py
@@ -1,4 +1,7 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,29 +9,33 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid = 5, stretch_len = 1)
-paddle.penup()
-paddle.goto(350, 0)
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-    
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((-350,0))
+l_paddle = Paddle((350,0))
+
+ball = Ball()
+
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "w")
+screen.onkey(r_paddle.go_down, "s")
+screen.onkey(l_paddle.go_up, "Up")
+screen.onkey(l_paddle.go_down, "Down")
 
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+        
+    #detech collision
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        print("made contact")
+        ball.bounce_x()
 
 
 
