216fa8fae8adab891f6c3393a6ea43ade5c6ba5f
joshdels <deleonjoshy2@gmail.com>
2025-04-22 23:22:44 +0800

    update tinder bot

---
+++ Day 50 - Tinder Bot/tinder_bot.py
@@ -0,0 +1,15 @@
+import os 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(chrome_options)
+driver.get("https://tinder.com/")
+
+
+
+
+
