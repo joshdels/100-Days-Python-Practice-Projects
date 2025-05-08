e750d152d5e9cba050afba389c0be0d0aeb5a2c9
joshdels <deleonjoshy2@gmail.com>
2025-05-08 17:05:52 +0800

    done day 59 sheesh website blog spot

--- Day 59/startbootstrap-clean-blog-gh-pages/startbootstrap-clean-blog-gh-pages/main.py
+++ Day 59/startbootstrap-clean-blog-gh-pages/startbootstrap-clean-blog-gh-pages/main.py
@@ -1,19 +1,33 @@
 from flask import Flask, render_template, url_for
-
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+url = "https://api.npoint.io/674f5423f73deab1e9a7"
+response = requests.get(url=url)
+all_post = response.json()
 
-@app.route("/about.html")
+name = "Joshua De Leon"
+
+@app.route("/index.html")
+def home():
+    return render_template('index.html', data=all_post, name=name)
+
+@app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/contact.html")
+@app.route("/contact")
 def contact():
     return render_template('contact.html')
+
+@app.route("/post.html/<int:post_id>")
+def get_post(post_id):
+    requested_post = None
+    for post in all_post:
+        if post['id'] == post_id:
+             requested_post = post
+    return render_template('post.html', data=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
