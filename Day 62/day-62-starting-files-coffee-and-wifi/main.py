089c2ada66fbdb5e127323e40b08849e99f5cd50
Joshua De Leon <deleonjoshy2@gmail.com>
2025-05-16 22:18:35 +0800

    practice sqlite

--- Day 62/day-62-starting-files-coffee-and-wifi/main.py
+++ Day 62/day-62-starting-files-coffee-and-wifi/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import csv
 
@@ -20,17 +20,11 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap5(app)
+Bootstrap(app)
 
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = StringField('Location URL', validators=[DataRequired()])
-    open_time = StringField('Open Time', validators=[DataRequired()])
-    close_time = StringField('Closing Time', validators=[DataRequired()])
-    coffee_rating = SelectField('Coffee Rating', choices=['☕', '☕☕', '☕☕☕', '☕☕☕☕', '☕☕☕☕☕'] )
-    wifi_rating = SelectField('Wifi Rating', choices=['✖','💪','💪💪','💪💪💪','💪💪💪💪','💪💪💪💪💪'])
-    power_outlet = SelectField('Power Outlet Rating Fields', choices=['✖','🔌','🔌🔌', '🔌🔌🔌','🔌🔌🔌🔌','🔌🔌🔌🔌🔌',])
     submit = SubmitField('Submit')
 
 # Exercise:
@@ -48,31 +42,20 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/add')
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        with open('Day 62/day-62-starting-files-coffee-and-wifi/cafe-data.csv', mode='a', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerow([
-                       form.location.data,
-                       form.open_time.data,
-                       form.close_time.data,
-                       form.coffee_rating.data,
-                       form.wifi_rating.data,
-                       form.power_outlet.data,
-            ])
-        return redirect(url_for('cafes'))
+        print("True")
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
-    
     return render_template('add.html', form=form)
 
 
 @app.route('/cafes')
 def cafes():
-    with open('Day 62/day-62-starting-files-coffee-and-wifi/cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
         for row in csv_data:
--- Day 63/day-63-starting-files-library-project/main.py
+++ Day 63/day-63-starting-files-library-project/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -7,14 +9,26 @@ all_books = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    pass
+    if request.method == 'POST': 
+        name = request.form['name'] 
+        author = request.form['author'] 
+        rating = request.form['rating'] 
+        all_books.append({"title":name, "author":author, "rating":rating})
+        
+        return redirect(url_for('home'))
+   
+    return render_template('add.html') 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# NOTE
+# read the document rather than stack over flow or chatgpt, try to read for few minutes before going deep
 
