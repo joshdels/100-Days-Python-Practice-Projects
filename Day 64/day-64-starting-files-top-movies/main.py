53cdfb69fcd8988bcd863cf7788ebebfb35ea140
joshdels <deleonjoshy2@gmail.com>
2025-05-27 12:44:33 +0800

    day 64 struggles

--- Day 64/day-64-starting-files-top-movies/main.py
+++ Day 64/day-64-starting-files-top-movies/main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired
 import requests
 
@@ -16,7 +16,11 @@ Bootstrap5(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Movie.db"
 
-
+class EditForm(FlaskForm):
+    rating = FloatField('Rating', validators=[DataRequired()])
+    review = StringField('Review', validators=[DataRequired()])
+    submit = SubmitField('Done') 
+    
 class Base(DeclarativeBase):
     pass
 
@@ -41,13 +45,13 @@ with app.app_context():
 
 # # ADD Data for test
 #     second_movie = Movies(
-#         title="Avatar The Way of Water",
-#         year=2022,
-#         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#         rating=7.3,
-#         ranking=9,
-#         review="I liked the water.",
-#         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+#         title="China Wars",
+#         year=1950,
+#         description="War on china is devastitating",
+#         rating=5.3,
+#         ranking=3,
+#         review="Hmmm mid tier.",
+#         img_url="https://i0.wp.com/asiatimes.com/wp-content/uploads/2020/05/China-War-Propaganda-Poster.jpg?fit=1400%2C900"
 #     )
     
 #     db.session.add(second_movie)
@@ -55,10 +59,33 @@ with app.app_context():
     
 @app.route("/")
 def home():
-    # ganna finish this query tomorrow nasad aron dili ma drain
-    all_movies = db.session.query(Movies).all()
-    return render_template("index.html")
+    # all_movies = db.session.query(Movies).all() --> tinapulan, pero same gihapon 
+    result = db.session.execute(db.select(Movies))
+    all_movies = result.scalars().all()
+    
+    return render_template("index.html", movies = all_movies)
 
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    form = EditForm()
+    movie = db.session.execute(db.select(Movies).where(Movies.id == id)).scalar()
+    if form.validate_on_submit():
+        movie.review = form.review.data
+        movie.rating = form.rating.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    
+    return render_template("edit.html", form=form)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    movie = db.session.execute(db.select(Movies).where(Movies.id == id)).scalar()
+    db.session.delete(movie)
+    db.session.commit()
+    
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+## Going edit table na josh! yeye hahha update
