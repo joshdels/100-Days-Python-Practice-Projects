b2a9df04613d47501775fdbfd89b72003ccb6795
joshdels <deleonjoshy2@gmail.com>
2025-07-29 23:38:26 +0800

    back to basics heheh

--- Day 67/day-67-starting-files-upgraded-blog/main.py
+++ Day 67/day-67-starting-files-upgraded-blog/main.py
@@ -39,7 +39,7 @@ class PostForm(FlaskForm):
 # CONFIGURE TABLE
 class BlogPost(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
     subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
     date: Mapped[str] = mapped_column(String(250), nullable=False)
     body: Mapped[str] = mapped_column(Text, nullable=False)
@@ -62,7 +62,7 @@ def get_all_posts():
     #udpate
 
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/show_post/<int:post_id>')
+@app.route('/show-post/<int:post_id>')
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
     requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id)).scalar()
@@ -70,7 +70,7 @@ def show_post(post_id):
 
 
 # TODO: add_new_post() to create a new blog post
-@app.route("/new_post", methods=["GET", "POST"])
+@app.route("/addnew_post", methods=["GET", "POST"])
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
@@ -87,16 +87,43 @@ def new_post():
         db.session.commit()
         return redirect(url_for('get_all_posts'))
     
+    
     return render_template("make-post.html", form=form)
     
     
 
-# TODO: edit_post() to change an existing blog post
-@app.route("/edit", methods=["GET", "POST"])
-def edit_post():
-    pass
+# # TODO: edit_post() to change an existing blog post
+@app.route("/edit_post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    print(post.title)
+    edit_form = PostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body,
+    )
+    if edit_form.validate_on_submit():
+        post.id = post.id
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.background_url.data
+        post.author = edit_form.authors_name.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+        
+    return render_template("make-post.html", form=edit_form, is_edit=True, post=post)
+    
 
 # TODO: delete_post() to remove a blog post from the database
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post = db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id)).scalar()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
